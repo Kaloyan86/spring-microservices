a9a95a69b07a0d3557a196635bb56643531a22d6
Kaloyan Kostadinov <kaloqn_kostadinow@abv.bg>
2022-08-06 16:03:46 +0300

    add api gateway-service

--- eureka-server/src/main/java/com/app/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/app/EurekaServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class Application {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
--- gateway-service/src/main/java/com/javainuse/APIGatewayApplication.java
+++ gateway-service/src/main/java/com/javainuse/APIGatewayApplication.java
@@ -1,15 +1,15 @@
-package com.app;
+package com.javainuse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient
-public class Application {
+@SpringBootApplication
+public class APIGatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(APIGatewayApplication.class, args);
+	}
 
 }
