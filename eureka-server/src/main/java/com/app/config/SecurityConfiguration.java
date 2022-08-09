538783b554d3001567e22e78c8dbcd58553233c6
Kaloyan Kostadinov <kaloqn_kostadinow@abv.bg>
2022-08-09 14:32:27 +0300

    create SecurityConfiguration of the eurkeka server, pass username and password into discovery services configurations (gateway-service and car-service)

---
+++ eureka-server/src/main/java/com/app/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf()
+            .disable()
+            .authorizeHttpRequests((authz) -> authz.anyRequest()
+                                                   .authenticated())
+            .httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
